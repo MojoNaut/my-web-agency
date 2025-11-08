d48a4621a7c41cb43fe8101ea7f03b0afe9b48bd
mojonaut <lorenzolivoti0690@gmail.com>
2025-11-08 16:43:41 +0100

    fix sanity basePath + clean dist

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -3,14 +3,14 @@
 import {defineConfig} from 'sanity';
 import {deskTool} from 'sanity/desk';
 import {visionTool} from '@sanity/vision';
-import {schemaTypes} from './schemaTypes';  // Named import
+import {schemaTypes} from './schemaTypes'; 
 
 export default defineConfig({
   projectId: process.env.SANITY_STUDIO_PROJECT_ID,
   dataset: process.env.SANITY_STUDIO_DATASET,
   title: 'Dotships Studio',
   apiVersion: '2023-01-01',
-  basePath: '/',        // FIX definitivo
+  basePath: '/',       
   plugins: [deskTool(), visionTool()],
   schema: { types: schemaTypes },
 })
