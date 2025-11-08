966db7990ab4f6735e1028112f5e91382628e8ed
mojonaut <lorenzolivoti0690@gmail.com>
2025-11-08 14:52:20 +0100

    fix: commented unused toggleTheme to unblock CI build

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+// client/src/App.js 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
@@ -11,9 +12,10 @@ export const ThemeContext = createContext(null);
 function App() {
   const [theme, setTheme] = useState("light");
 
-  const toggleTheme = () => {
-    setTheme((curr) => (curr === "light" ? "dark" : "light"));
-  }
+ // const toggleTheme = () => {
+//   setTheme((curr) => (curr === "light" ? "dark" : "light"));
+// }
+
   return (
     <ThemeContext.Provider value={{ theme, setTheme}}>
     <Router>
